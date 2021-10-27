893f97bbd0575a7b3faebb0397ac66bee810afb5
Riley McWhorter <rmcwhorter@ponsys.com>
2021-10-26 19:22:53 -0500

    testingTables

--- backend/seed.js
+++ backend/seed.js
@@ -6,12 +6,12 @@ const createDB = async () => {
     console.log(client.query);
     await client.query(
       `
-    DROP TABLE IF EXISTS products;
-    DROP TABLE IF EXISTS categories; 
-    DROP TABLE IF EXISTS products_orders;
-    DROP TABLE IF EXISTS users;
-    DROP TABLE IF EXISTS orders;    
-    DROP TABLE IF EXISTS reviews;
+      DROP TABLE IF EXISTS reviews CASCADE;       
+      DROP TABLE IF EXISTS products CASCADE;
+      DROP TABLE IF EXISTS orders CASCADE; 
+      DROP TABLE IF EXISTS product_orders CASCADE;
+      DROP TABLE IF EXISTS users CASCADE;
+      DROP TABLE IF EXISTS categories CASCADE;
 
     
     CREATE TABLE categories(
