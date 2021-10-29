74acb7fc035b46a209baa837831802867e4d7cd9
Riley McWhorter <rmcwhorter@ponsys.com>
2021-10-28 20:03:24 -0500

    rileyTables

--- backend/api/orders.js
+++ backend/api/orders.js
@@ -1,3 +1,14 @@
+const {
+  createOrders,
+  //   getProductById,
+  //   getProductOrdersByProduct,
+} = require("./db");
+
 const ordersRouter = require("express").Router();
+
+ordersRouter.get("/", async (req, res) => {
+  const orders = await createOrders();
+  res.send(orders);
+});
 
 module.exports = ordersRouter;
