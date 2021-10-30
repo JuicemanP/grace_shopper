12a40dd2a024e9ed1f6cb54aaaa288b8b3c07c50
Riley McWhorter <rmcwhorter@ponsys.com>
2021-10-30 12:36:31 -0500

    rileyTables

--- backend/api/orders.js
+++ backend/api/orders.js
@@ -1,48 +1,8 @@
-// const {
-//   createOrders,
-//   getProductById,
-//   getProductOrdersByProduct,
-// } = require("../db");
-
-// const ordersRouter = require("express").Router();
-
-// ordersRouter.get("/", async (req, res) => {
-//   const order = await createOrders();
-
-//   res.send(order);
-// });
-
-// ordersRouter.post("/registerOrder", async (req, res) => {
-//   const { user_id, status } = req.body;
-//   const productId = await createOrders({ user_id, status });
-//   res.send(productId);
-// });
-
-// ordersRouter.get("/", async (req, res) => {
-//   const products = await getProductById();
-//   res.send(products);
-// });
-
-// ordersRouter.patch("/:ordersId", async (req, res) => {
-//   const { ordersId } = req.params;
-//   const { name, description } = req.body;
-//   const updatedOrders = await updateOrders({
-//     id: ordersId,
-//     name,
-//     description,
-//   });
-//   console.log(updatedOrders);
-//   res.send(updatedOrders);
-// });
-
-// ordersRouter.get("/:ordersId", async (req, res) => {
-//   const productId = await getProductOrdersByProduct();
-//   res.send(productId);
-// });
-
-// module.exports = ordersRouter;
-
-const { createOrders } = require("../db/orders");
+const {
+  createOrders,
+  getProductById,
+  getProductOrdersByProduct,
+} = require("../db/orders");
 
 const ordersRouter = require("express").Router();
 
@@ -55,4 +15,33 @@ ordersRouter.post("/", async (req, res) => {
     throw error;
   }
 });
+
+// ordersRouter.get("/:ordersId", async (req, res) => {
+//   try {
+//     const id = req.params.ordersId();
+//     const productId = await getProductOrdersByProduct(id);
+//     res.send(productId);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// });
+
+ordersRouter.get("/:userid/products", async (req, res) => {
+  try {
+    const userId = req.params.userid;
+    const products = await getProductById(userId);
+    res.send(products.title);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
 module.exports = ordersRouter;
+
+//getting order by userid
+// by order_id
+
+//patch
+//product to order
+
+//delete Orders
