6df23bf2fc6aa8c85a26b91d7a9969dd4eb9ff91
unknown <kimjimmyy@yahoo.com>
2021-11-02 19:14:11 -0500

    starting

--- backend/api/orders.js
+++ backend/api/orders.js
@@ -1,3 +1,27 @@
+const { createOrders,getProductById } = require("../db/orders");
+
 const ordersRouter = require("express").Router();
 
+ordersRouter.post("/", async(req,res)=>{
+    
+
+    try {
+        const { user_id, status}=req.body;
+        const newOrder=await createOrders({user_id,status})
+        res.send(newOrder) 
+    } catch (error) {
+        throw(error)
+    }
+})
+
+ordersRouter.get("/", async(req,res)=>{
+
+    try {
+        const orderProducts=await getProductById()
+       
+        res.send({orderProducts}) 
+    } catch (error) {
+        throw(error)
+    }
+})
 module.exports = ordersRouter;
