21b0455641e48ee392795470a795e6be6a514e63
unknown <kimjimmyy@yahoo.com>
2021-11-06 12:37:20 -0500

    categories

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,28 @@
 import { Link } from "react-router-dom"
 
-const Navbar = ()=>{
-    return <div>
-<Link to = "/"> Home </Link> ||
-<Link to = "/register"> Register</Link> || 
-<Link to = "/login"> Login</Link> ||
-<Link to ="/logout"> Logout</Link>
+const Navbar = (props)=>{
+
+
+    return <div >
+<div className="dropdown">
+<Link to = "/"> Home </Link> 
+    <div class="dropdown-content">
+    <Link>Categories</Link>
+        <div class="dropdown-content">
+        <Link>Men</Link>
+        <Link>Women</Link>
+        <Link>Children</Link>
+        </div>
+    </div>
+</div>
+
+<Link>
+<input  placeholder="Search..."/>
+</Link>
+<Link>Cart</Link>
+{!props.user && <Link to = "/login"> Login</Link>} 
+{!props.user && <Link to = "/register"> Register</Link>} 
+{props.user && <Link to ="/logout"> Logout</Link>} 
     </div>
 }
 
