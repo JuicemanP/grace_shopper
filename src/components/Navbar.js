9e67c54f2cbf601a117bfdac3ef68485733f0364
unknown <kimjimmyy@yahoo.com>
2021-11-15 19:59:04 -0600

    fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,17 @@
 import { Link } from "react-router-dom";
 import { useHistory } from "react-router";
+import React, { useState } from "react";
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+
+import { SidebarData } from "./SidebarData";
+import "./Navbar.css";
+import { IconContext } from "react-icons";
 
 const Navbar = (props) => {
+  const [sidebar, setSidebar] = useState(false);
   const { jerseys, setJerseys } = props;
+  const showSidebar = () => setSidebar(!sidebar);
 
   const history = useHistory();
 
@@ -16,32 +25,59 @@ const Navbar = (props) => {
   };
 
   return (
-    <div>
-      <header>
-      <div className="dropdown">
-        <Link to="/"> Home </Link>
-        <div class="dropdown-content">
-          <h2>Categories</h2>
-          <div class="dropdown-content">
-            <Link>Men</Link>
-            <Link>Women</Link>
-            <Link>Children</Link>
-          </div>
+    <>
+      {" "}
+      <nav>
+        <ul className="nav-links">
+          <li> {!props.user && <a href="/login"> Login</a>}</li>{" "}
+          <li>{!props.user && <a href="/register">Register</a>}</li>
+          <li>
+            {" "}
+            {props.user && (
+              <a href="/" onClick={handleLogout}>
+                Logout
+              </a>
+            )}
+          </li>
+          <li>
+            <Link to="/cart">
+              <FaIcons.FaCartPlus className="cart-icon" />
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <IconContext.Provider value={{ color: "#fff" }}>
+        <div className="navbar">
+          {" "}
+          <Link to="#" className="menu-bars">
+            <FaIcons.FaBars onClick={showSidebar} />
+          </Link>{" "}
+        </div>{" "}
+        <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
+          <ul className="nav-menu-items" onClick={showSidebar}>
+            {" "}
+            <li className="navbar-toggle">
+              <Link to="#" className="menu-bars">
+                <AiIcons.AiOutlineClose />
+              </Link>
+            </li>
+            {SidebarData.map((item, index) => {
+              return (
+                <li key={index} className={item.cName}>
+                  <Link to={item.path}>
+                    {item.icon}
+                    <span>{item.title}</span>
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>{" "}
+        <div>
+          <a href="/login" className="login"></a>
         </div>
-      </div>
-     
-      <Link to="/products">Shop Jerseys</Link>
-      <Link to="/cart">My Cart</Link>
-      {!props.user && <Link to="/login"> Login</Link>}
-      {!props.user && <Link to="/register"> Register</Link>}
-      {props.user && (
-        <Link to="/" onClick={handleLogout}>
-          Logout
-        </Link>
-      )}
-      </header>
-      
-    </div>
+      </IconContext.Provider>
+    </>
   );
 };
 
